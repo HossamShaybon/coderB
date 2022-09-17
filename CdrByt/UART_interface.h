cd1575b16e248a7fa653ead9561f739af246c673
Hossam Shaybon <87588994+HossamShaybon@users.noreply.github.com>
2022-09-17 15:29:16 +0200

    Update UART_interface.h

--- CdrByt/UART_interface.h
+++ CdrByt/UART_interface.h
@@ -1,34 +1,17 @@
-############# UART.c ##############
-#include "UART_interface.h"
-uint32_t baudrate= 51
-void SWUART_init(uint32_t baudrate){
-	UCSRA=0;
-	UCSRA|=0<<1;
-	UCSRA|=0<<0;
-	/*
-	 * select the character size in bit 2
-	 */
-	CLR_BIT(UCSRB, UCSRB_UCSZ2);
 
-	UCSRC = 0b10101110;
-	/*
-	 * Set the Baud Rate 9600 bps
-	 */
-	UBRRL = baudrate ;
+############# UART_interface.h ##############
+/*
+ * baudrate: is an input argument that describes baudrate that the UART needs to make the communications.
+ */
+ void SWUART_init(uint32_t baudrate);
 
-	/*
-	 * Enable Rx, Tx, better enabling at end
-	 */
-	SET_BIT(UCSRB, UCSRB_TXEN);
-	SET_BIT(UCSRB, UCSRB_RXEN);
-}
-
-
- void SWUART_send(uint8_t data){
-	while (GET_BIT(UCSRA,UCSRA_UDRE) == 0);
-	UDR = copy_u8Data;
-}
-void SWUART_recieve(uint8_t *data) {
-	while (GET_BIT(UCSRA,UCSRA_RXC) == 0);
-	data=UDR;
-}
+/*
+ * data: is an input argument that describes a byte of data to be send over the SW UART.
+ */
+ void SWUART_send(uint8_t data);
+ 
+ /*
+ * data: is an output argument that describes a byte of data to be recieved by the SW UART.
+ */
+ void SWUART_recieve(uint8_t *data);
+//////////////////////////////////////////////////////////
